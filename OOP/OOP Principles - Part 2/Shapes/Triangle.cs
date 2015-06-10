beda8832ed863d3d56ce1b791884d78c047221d9
K.Popov <Yumofsc@gmail.com>
2015-06-11 00:24:58 +0300

    one last

---
+++ OOP/OOP Principles - Part 2/Shapes/Triangle.cs
@@ -0,0 +1,25 @@
+﻿namespace Shapes
+{
+    //Define two new class Triangle that implement the 
+    //virtual method and return the surface of the figure (height * width/2).
+    using System;
+
+    public class Triangle : Shape
+    {
+        public Triangle(double width, double height)
+           : base(width,height)
+        {
+
+        }
+
+        public override double CalculateSurface()
+        {
+            double surface = (this.Height * this.Width)/2;
+            return surface;
+        }
+        public override string ToString()
+        {
+            return this.GetType().Name;
+        }
+    }
+}
